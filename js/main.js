0c6d5c1ce3685b2e3118545ab03b375556a8dba1
Variniia Zavadskaia <variniyaal@gmail.com>
2024-05-29 21:40:32 +0300

    Random diameter

--- js/main.js
+++ js/main.js
@@ -4,19 +4,26 @@
 //     console.log('Green ball');
 // }
 var gBallSize = 100;
+
+// function onBallClick() {
+//     var elBall = document.querySelector('.ball');
+//     if (gBallSize < 400) {
+//         gBallSize += 50
+//         elBall.style.height = gBallSize + 'px';
+//         elBall.style.width = gBallSize + 'px';
+//     } else {
+//         gBallSize = 100;
+//         elBall.style.height = gBallSize + 'px';
+//         elBall.style.width = gBallSize + 'px';
+//         // document.querySelector('.ball').style.transition = 1 + 's'
+
+//     }
+// }
+
 function onBallClick() {
     var elBall = document.querySelector('.ball');
-    if (gBallSize < 400) {
-        gBallSize += 50
-        elBall.style.height = gBallSize + 'px';
-        elBall.style.width = gBallSize + 'px';
-    } else {
-        gBallSize = 100;
-        elBall.style.height = gBallSize + 'px';
-        elBall.style.width = gBallSize + 'px';
-        // document.querySelector('.ball').style.transition = 1 + 's'
-
-    }
+    var size = getRandomInt(20, 60) + 'px';
+    elBall.style.height = size;
+    elBall.style.width  = size;
 }
-
 
