67e0b223f88f8796cb58d85da29abd2e42aa16bf
Variniia Zavadskaia <variniyaal@gmail.com>
2024-05-29 16:37:23 +0300

    Update ball's size

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 'use strict'
-function onBallClick(){
-    console.log('Green ball');
+
+// function onBallClick(){
+//     console.log('Green ball');
+// }
+
+function onBallClick() {
+    var elBall = document.querySelector('.ball');
+    elBall.style.height = 50 + 'px';
+    elBall.style.width = 50 + 'px';
+    elBall.innerHTML;
 }
 
 
